667a6f58c0404ef55c69dfe01964a5a52b2a5ad0
Vinit  Varghese <Vinit@Vinits-MBP-2.T-mobile.com>
2017-08-16 13:01:16 -0700

    UI added alert messages

--- components/scripts/script1.js
+++ components/scripts/script1.js
@@ -1,107 +1,121 @@
-var DEditor = angular.module("DEditor", ['ngResource', 'ui.bootstrap','ngSanitize']);
+var DEditor = angular.module("DEditor", ['ngResource', 'ui.bootstrap', 'ngSanitize']);
 
-    DEditor.config(['$httpProvider', function($httpProvider) {
-        $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=utf-8';
-    }]);
+DEditor.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=utf-8';
+}]);
 
-    DEditor.controller("DEditorCtrl", function($scope) {
-        $scope.dkey = "Vinit Varghese";
-    });
-
-
-
-      DEditor.controller("KeyValCtrl", ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
-        $scope.dictionarydata = [];
+DEditor.controller("DEditorCtrl", function($scope) {
+    $scope.dkey = "Vinit Varghese";
+});
 
 
 
-        $scope.sendpost = function() {
+DEditor.controller("KeyValCtrl", ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
+    $scope.dictionarydata = [];
+
+
+
+    $scope.sendpost = function() {
         var data = "aa";
-        var ss=$('#selectSilo').val();
-        $http.get("https://api-wpm"+ss+".apicasystem.com/v3/scenarios/proxysniffer/dictionaries/"+$scope.dikey+"?auth_ticket="+$scope.apiKey).success(function(data, status) {
-            //$scope.hello = data;
-            var result = data;
-            $scope.dictionarydata=[];
+        var ss = $('#selectSilo').val();
+        $http.get("https://api-wpm" + ss + ".apicasystem.com/v3/scenarios/proxysniffer/dictionaries/" + $scope.dikey + "?auth_ticket=" + $scope.apiKey)
+
+            .then(function(data, status) {
+                //$scope.hello = data;
+                var result = data.data;
+                $scope.dictionarydata = [];
                 $scope.dkey = '';
                 $scope.dvalue = '';
-            $.each(result, function(k, v) {
+                $.each(result, function(k, v) {
 
-                $scope.dictionarydata.push({ 'dkey': k, 'dvalue': v});
-            });
-            console.log(data);
-        })
-         };
-
-
-        $scope.addRow = function() {
-            $scope.statusmessage='';
-            var mindex = $scope.dictionarydata.findIndex(item => item.dkey === $scope.dkey);
-            if(mindex  === -1){
-                
-                $scope.dictionarydata.push({ 'dkey': $scope.dkey, 'dvalue': $scope.dvalue });
-                $scope.dkey = '';
-                $scope.dvalue = '';
-            }
-            else 
-            {
-                $scope.statusmessage = "<div style='width:300px;' class='alert alert-warning' role='alert'>Key '"+$scope.dkey+"' already exists in the dictionary</div>";
-                $timeout(function() {
-                  $scope.statusmessage = '';
-                }, 4500);
-            }
-        };
-
-        $scope.addRowAsyncAsNV = function() {
-            $scope.dictionarydata.push({ 'dkey': $scope.dkey, 'dvalue': $scope.dvalue });
-            // Writing it to the server
-            //		
-            var data = 'dkey=' + $scope.dkey + '&dvalue=' + $scope.dvalue;
-            $http.post('/savecompany', data)
-                .success(function(data, status, headers, config) {
-                    $scope.message = data;
-                })
-                .error(function(data, status, headers, config) {
-                    alert("failure message: " + JSON.stringify({ data: data }));
+                    $scope.dictionarydata.push({ 'dkey': k, 'dvalue': v });
                 });
-            // Making the fields empty
-            //
+                $scope.addStatus("Dictionary Loaded Successfully!","success", 3000);
+            })
+            .catch(function(data) {
+                console.log(data);
+                if (data.status == 404) {
+                    $scope.addStatus(data.data.Message,"danger", 3000);
+
+                } else {
+                    $scope.addStatus(data.data,"danger", 3000);
+                }
+            });
+    };
+
+
+    $scope.addRow = function() {
+        $scope.statusmessage = '';
+        var mindex = $scope.dictionarydata.findIndex(item => item.dkey === $scope.dkey);
+        if (mindex === -1) {
+
+            $scope.dictionarydata.push({ 'dkey': $scope.dkey, 'dvalue': $scope.dvalue });
             $scope.dkey = '';
             $scope.dvalue = '';
-            $scope.headoffice = '';
-        };
+        } else {
+            $scope.addStatus("Key '" + $scope.dkey + "' already exists in the dictionary", "warning", 4500);
+        }
+    };
 
-        $scope.removeRow = function(dkey) {
-            var index = -1;
-            var comArr = eval($scope.dictionarydata);
-            for (var i = 0; i < comArr.length; i++) {
-                if (comArr[i].dkey === dkey) {
-                    index = i;
-                    break;
-                }
+    $scope.addRowAsyncAsNV = function() {
+        $scope.dictionarydata.push({ 'dkey': $scope.dkey, 'dvalue': $scope.dvalue });
+        // Writing it to the server
+        //      
+        var data = 'dkey=' + $scope.dkey + '&dvalue=' + $scope.dvalue;
+        $http.post('/savecompany', data)
+            .success(function(data, status, headers, config) {
+                $scope.message = data;
+            })
+            .error(function(data, status, headers, config) {
+                alert("failure message: " + JSON.stringify({ data: data }));
+            });
+        // Making the fields empty
+        //
+        $scope.dkey = '';
+        $scope.dvalue = '';
+        $scope.headoffice = '';
+    };
+
+    $scope.removeRow = function(dkey) {
+        var index = -1;
+        var comArr = eval($scope.dictionarydata);
+        for (var i = 0; i < comArr.length; i++) {
+            if (comArr[i].dkey === dkey) {
+                index = i;
+                break;
             }
-            if (index === -1) {
-                alert("Something gone wrong");
+        }
+        if (index === -1) {
+            alert("Something gone wrong");
+        }
+        $scope.dictionarydata.splice(index, 1);
+    };
+
+
+    $scope.addStatus = function(statusmessage, statustype, statustimer) {
+        $scope.statusmessage = "<div style='width:300px;' class='alert alert-" + statustype + "' role='alert'>" + statusmessage + "</div>";
+        $timeout(function() {
+            $scope.statusmessage = '';
+        }, statustimer);
+    };
+
+
+    $scope.editRow = function(dkey) {
+        var index = -1;
+        var comArr = eval($scope.dictionarydata);
+        for (var i = 0; i < comArr.length; i++) {
+            if (comArr[i].dkey === dkey) {
+                index = i;
+                break;
             }
-            $scope.dictionarydata.splice(index, 1);
-        };
-
-         $scope.editRow = function(dkey) {
-            var index = -1;
-            var comArr = eval($scope.dictionarydata);
-            for (var i = 0; i < comArr.length; i++) {
-                if (comArr[i].dkey === dkey) {
-                    index = i;
-                    break;
-                }
-            }
-            if (index === -1) {
-                alert("Something gone wrong");
-            }
-           console.log($scope.dictionarydata[index].dkey);
-            $scope.dkey =  $scope.dictionarydata[index].dkey;
-            $scope.dvalue = $scope.dictionarydata[index].dvalue;
-        };
+        }
+        if (index === -1) {
+            alert("Something gone wrong");
+        }
+        console.log($scope.dictionarydata[index].dkey);
+        $scope.dkey = $scope.dictionarydata[index].dkey;
+        $scope.dvalue = $scope.dictionarydata[index].dvalue;
+    };
 
 
-    }]);
-
+}]);
